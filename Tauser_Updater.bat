514962b08c3771c3a65895f2edc5284a967aa9a2
FBW81C <122120714+FBW81C@users.noreply.github.com>
2023-03-01 19:00:48 +0100

    Update Tauser_Updater.bat

--- Tauser_Updater.bat
+++ Tauser_Updater.bat
@@ -6,9 +6,10 @@ timeout 1 >NUL
 rem verion datei
 %windir%\TauserTool\wget\wget.exe https://raw.githubusercontent.com/FBW81C/TauserTool/main/version.sys -O%windir%\TauserTool\version.sys
 type nul>empty.sys
-fc empty.sys %windir%\TauserTool\version.sys
+fc empty.sys %windir%\TauserTool\version.sys >NUL
 if %errorlevel%==0 goto fail
 
+rem Test version datei
 fc %windir%\TauserTool\version.sys %windir%\TauserTool\local_version.sys >NUL
 if not %errorlevel%==0 goto download_update
 echo You are already on the newest version.
@@ -18,7 +19,7 @@ goto finish
 :download_update
 %windir%\TauserTool\wget\wget.exe https://raw.githubusercontent.com/FBW81C/TauserTool/main/Tauser_Tool.bat -O%windir%\TauserTool\Tauser_Tool.update
 type nul>empty.sys
-fc empty.sys %windir%\TauserTool\Tauser_Tool.update
+fc empty.sys %windir%\TauserTool\Tauser_Tool.update >NUL
 if %errorlevel%==0 goto fail
 del %windir%\TauserTool\Tauser_Tool_backup.bat
 ren %windir%\TauserTool\Tauser_Tool.bat Tauser_Tool_backup.bat
