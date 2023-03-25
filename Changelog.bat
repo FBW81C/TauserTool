701dff51db1573b60b89c7e436f7cefeb122560b
FBW81C <122120714+FBW81C@users.noreply.github.com>
2023-03-25 17:43:31 +0100

    Update Changelog.bat

--- Changelog.bat
+++ Changelog.bat
@@ -2,6 +2,9 @@
 
 echo This is the changelog form Tauser Tool
 echo Herer is a list of new features or bug fixes
+echo Tipp: +) = Added something new
+echo Tipp: -) = Removed something
+echo Tipp: *) = Fixed something
 echo +) Changelog
 echo +) Updatable Setup
 pause
